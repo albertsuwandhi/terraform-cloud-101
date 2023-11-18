df80e8782ac4db65fd74a2950b5fddfbf368e46f
Albert Suwandhi <albert.suwandhi@gmail.com>
2023-11-18 10:35:10 +0700

    Delete All

--- s3/main.tf
+++ s3/main.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "terraform-cloud-101-s3-01" {
-  bucket = "s3-terraform-1337-001"
+# resource "aws_s3_bucket" "terraform-cloud-101-s3-01" {
+#   bucket = "s3-terraform-1337-001"
 
-  tags = {
-    env = "${var.env}",
-    createdby = "terraform"
-  }
-}
+#   tags = {
+#     env = "${var.env}",
+#     createdby = "terraform"
+#   }
+# }
 
 # resource "aws_s3_bucket" "terraform-cloud-101-s3-02" {
 #   bucket = "s3-terraform-1337-02"
@@ -16,25 +16,25 @@ resource "aws_s3_bucket" "terraform-cloud-101-s3-01" {
 #   }
 # }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0a72af05d27b49ccb"
-  instance_type = "t2.micro"
+# resource "aws_instance" "example" {
+#   ami           = "ami-0a72af05d27b49ccb"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "EC2-${var.env}-01",
-    createdby = "terraform"
-  }
+#   tags = {
+#     Name = "EC2-${var.env}-01",
+#     createdby = "terraform"
+#   }
 
-}
+# }
 
-resource "aws_instance" "example2" {
-  ami           = "ami-0a72af05d27b49ccb"
-  instance_type = "t3.medium"
+# resource "aws_instance" "example2" {
+#   ami           = "ami-0a72af05d27b49ccb"
+#   instance_type = "t3.medium"
 
-  tags = {
-    Name = "EC2-${var.env}-002",
-    createdby = "terraform"
-  }
+#   tags = {
+#     Name = "EC2-${var.env}-002",
+#     createdby = "terraform"
+#   }
 
-}
+# }
 
